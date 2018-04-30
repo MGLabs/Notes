da3ed1dbc058c2143c6ef13139e0d08e9b8e8283
MGLabs <goldwings3@hotmail.com>
2018-04-30 18:51:37 +0200

    DetailActivity added

--- app/src/main/java/com/mglabs/notes/MainActivity.java
+++ app/src/main/java/com/mglabs/notes/MainActivity.java
@@ -2,12 +2,38 @@ package com.mglabs.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        notes.add("Example Note");
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes );
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+
+
     }
 }
