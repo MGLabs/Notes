980d1ecf214f19ed4b61a0da65e6411e2faf723b
MGLabs <goldwings3@hotmail.com>
2018-04-30 19:52:52 +0200

    onItem selected added

--- app/src/main/java/com/mglabs/notes/DetailActivity.java
+++ app/src/main/java/com/mglabs/notes/DetailActivity.java
@@ -1,13 +1,27 @@
 package com.mglabs.notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DetailActivity extends AppCompatActivity {
+
+    EditText noteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        noteText = (EditText) findViewById(R.id.editText);
+
+        Intent intent = getIntent();
+        int noteId = intent.getIntExtra("noteId", -1);
+
+        if (noteId != -1) {
+            noteText.setText(MainActivity.notes.get(noteId));
+        }
+
     }
 }
--- app/src/main/java/com/mglabs/notes/MainActivity.java
+++ app/src/main/java/com/mglabs/notes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mglabs.notes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> notes = new ArrayList<>();
+    static ArrayList<String> notes = new ArrayList<>();
     ArrayAdapter adapter;
 
     @Override
@@ -23,16 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         notes.add("Example Note");
 
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes );
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                intent.putExtra("noteId", position);
+                startActivity(intent);
             }
         });
-
 
 
     }
